9dbb5bc5df439a44544d0ede970ee45cb53f9e78
Beat Richartz <beat.richartz@gmail.com>
2015-01-07 09:57:35 +0100

    more readable implementation of blank object

--- lib/configurations/blank_object.rb
+++ lib/configurations/blank_object.rb
@@ -3,23 +3,60 @@ module Configurations
   # Create a blank object with some kernel methods
   #
   class BlankObject < ::BasicObject
-    OBJECT_METHODS  = [:equal?, :__id__, :__send__, :method_missing].freeze
+    # The instance methods to keep on the blank object.
+    #
+    KEEP_METHODS = [
+      :equal?,
+      :__id__,
+      :__send__,
+      :method_missing
+    ].freeze
 
-    ALIASED_KERNEL_METHODS = {__class__: :class, __instance_eval__: :instance_eval, __define_singleton_method__: :define_singleton_method}.freeze
-    KERNEL_METHODS  = [:respond_to?, :is_a?, :kind_of?, :inspect, :object_id].freeze
+    # The kernel methods to alias to an internal name
+    #
+    ALIAS_KERNEL_METHODS = {
+      __class__: :class,
+      __instance_eval__: :instance_eval,
+      __define_singleton_method__: :define_singleton_method
+    }.freeze
 
-    (::BasicObject.instance_methods - OBJECT_METHODS).each do |method|
+    # The kernel methods to keep on the blank object
+    #
+    KEEP_KERNEL_METHODS  = [
+      :respond_to?,
+      :is_a?,
+      :inspect,
+      :object_id,
+      *ALIAS_KERNEL_METHODS.keys
+    ].freeze
+
+    # Undefines every instance method except the kept methods
+    #
+    (instance_methods - KEEP_METHODS).each do |method|
       undef_method method
     end
 
-    kernel = ::Kernel.dup
-    ALIASED_KERNEL_METHODS.each do |new_name, old_name|
-      kernel.module_eval { alias_method new_name, old_name }
+    # @return [Module] A Kernel module with only the methods defined in KEEP_KERNEL_METHODS
+    #
+    def self.blank_kernel
+      kernel = ::Kernel.dup
+
+      ALIAS_KERNEL_METHODS.each do |new_name, old_name|
+        kernel.module_eval do
+          alias_method new_name, old_name
+        end
+      end
+
+      (kernel.instance_methods - KEEP_KERNEL_METHODS).each do |method|
+        kernel.module_eval do
+          undef_method method
+        end
+      end
+
+      kernel
     end
-    (kernel.instance_methods - (KERNEL_METHODS + ALIASED_KERNEL_METHODS.keys)).each do |method|
-      kernel.__send__ :undef_method, method
-    end
-    include kernel
+
+    include blank_kernel
   end
 
 end
